0dfe509ba3bc1c52973156133c4207d11f685800
balasgit <bala@us.ibm.com>
2022-05-11 16:42:23 -0500

    updated stages test folder to sync with framework

--- test/stages/stage1-resource-group.tf
+++ test/stages/stage1-resource-group.tf
@@ -2,6 +2,9 @@ module "resource_group" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group.git"
 
   resource_group_name = var.resource_group_name
+
+
+  ## Added to reflect the RG module change
   ibmcloud_api_key    = var.ibmcloud_api_key
-  provision           = false
+
 }
